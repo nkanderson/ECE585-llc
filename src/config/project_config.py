e661ad70c73779dc3c9db71f901cb931dad53a56
Niklas Anderson <nkanders@gmail.com>
2024-11-11 08:46:20 -0800

    Add main program loop
    Adds a basic program loop and event handler to process each operation from the trace file. Also configures module-level instances of the logger and args, so they can be imported and used throughout the program.

--- src/config/project_config.py
+++ src/config/project_config.py
@@ -5,9 +5,54 @@ This module contains global configuration settings and constants for the LLC Cac
 
 """
 
+import argparse
 from pathlib import Path
+
+from common.constants import LogLevel
+from utils.cache_logger import CacheLogger
 
 ROOT_DIR = Path(__file__).parents[1]
 
 DATA_DIRECTORY = "data"
 DEFAULT_TRACE_FILE = "trace.txt"
+
+
+def parse_arguments():
+    """
+    Parse command line arguments for the trace file parser.
+
+    Returns:
+        argparse.Namespace: Parsed command line arguments
+    """
+    parser = argparse.ArgumentParser(
+        description="Cache Simulation Trace File Parser",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "-f", "--file", type=str, help="Path to the trace file to process", default=None
+    )
+    parser.add_argument(
+        "-s", "--silent", action="store_true", help="Reduce program output"
+    )
+    parser.add_argument(
+        "-d", "--debug", action="store_true", help="Enable debug output"
+    )
+    return parser.parse_args()
+
+
+def initialize():
+    """Set up configurations that depend on runtime conditions."""
+    # Declare globals so these can be accessed at the module level
+    global args, logger
+    args = parse_arguments()
+
+    if args.silent:
+        loglevel = LogLevel.SILENT
+    else:
+        loglevel = LogLevel.DEBUG if args.debug else LogLevel.NORMAL
+
+    # Create an instance of the logger to be shared across files
+    logger = CacheLogger(loglevel)
+
+
+initialize()
--- src/main.py
+++ src/main.py
@@ -7,12 +7,42 @@ Date: November, 2024
 Description: This program simulates the behavior of a 16MB, 16-way set associative last level cache.
 """
 
+from common.constants import LogLevel
+from config.project_config import args, logger
+from utils.event_handler import handle_event
+from utils.trace_parser import TraceFileParser
+
 
 def main():
     """
     Main entry point of the LLC Simulation program.
     """
-    print("LLC sim says hello from main program")
+    try:
+        with TraceFileParser(args.file) as parser:
+            while True:
+                result = parser.read_line()
+                if result is None:
+                    break
+                op, addr = result
+                logger.log(
+                    LogLevel.DEBUG,
+                    f"\nOperation: {op.value} {op.name:20} Address: 0x{addr:08x}",
+                )
+                # TODO: Update Stats instance with these values, once we
+                # decide where that instance lives
+                stats_changes = handle_event(op.value)
+                logger.log(
+                    LogLevel.DEBUG,
+                    f"Updating stats with the following: {stats_changes}",
+                )
+    # Log a meaningful message and re-raise error to exit the program
+    # with a non-zero exit code
+    except ValueError as e:
+        logger.log(LogLevel.DEBUG, f"[ERROR] - {str(e)}")
+        raise
+    except Exception as e:
+        logger.log(LogLevel.DEBUG, f"[ERROR] - An unexpected error occurred: {str(e)}")
+        raise
 
 
 if __name__ == "__main__":
