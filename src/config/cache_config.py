41f8de4e2905678f9e8d77c9cff39d54900f34bf
reecewayt <reecewayt96@gmail.com>
2024-11-21 16:47:46 -0800

    Updating cache_config and .env file to include address width of 32

--- src/config/cache_config.py
+++ src/config/cache_config.py
@@ -47,6 +47,10 @@ class CacheConfig:
         protocol = os.getenv("CACHE_PROTOCOL")
         if protocol is None:
             raise EnvironmentError("CACHE_PROTOCOL not set in .env file")
+        
+        address_size = os.getenv("ADDRESS_SIZE")
+        if address_size is None:
+            raise EnvironmentError("ADDRESS_SIZE not set in .env file")
 
         # Convert values
         self.total_capacity_mb = int(capacity)
@@ -54,6 +58,7 @@ class CacheConfig:
         self.line_size = int(line_size)
         self.associativity = int(associativity)
         self.protocol = protocol
+        self.address_size = int(address_size) # In bits
 
 
 """
