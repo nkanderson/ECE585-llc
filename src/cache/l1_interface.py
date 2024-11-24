af4d5e73a7dd768d995cf6963e405b7731a37a74
Niklas Anderson <nkanders@gmail.com>
2024-11-24 12:22:20 -0800

    Fixup L1 interface message log level

--- src/cache/l1_interface.py
+++ src/cache/l1_interface.py
@@ -29,7 +29,7 @@ class L1Interface:
 
     def message_to_cache(self, message: CacheMessage, address: int) -> None:
         """Send control message to L1 cache"""
-        self.logger.log(LogLevel.DEBUG, f"L2: {message.name}, Address: {address}")
+        self.logger.log(LogLevel.NORMAL, f"L2: {message.name}, Address: {address}")
 
 
 def message_to_l1_cache(message: CacheMessage, address: int) -> None:
