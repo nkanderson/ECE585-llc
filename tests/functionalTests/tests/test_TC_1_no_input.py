ea012af5e2655e4649c9021950c43f7570ce2373
Matthew Hardenburgh <matthew@hardenburgh.io>
2024-12-10 09:42:43 -0800

    saving work

--- tests/functionalTests/tests/test_TC_1_no_input.py
+++ tests/functionalTests/tests/test_TC_1_no_input.py
@@ -5,9 +5,9 @@ calls sim with no input
 '''
 def test():
     # Simple command without capturing output
-    #subprocess.run(['ls', '-l'])
+    subprocess.run(['ls', '-l'])
 
     # Command with output capture
-    #result = subprocess.run(['ls', '-l'], capture_output=True, text=True)
-    #print(result.stdout)  # Prints the command output
-    return False
+    result = subprocess.run(['ls', '-l'], capture_output=True, text=True)
+    print(result.stdout)  # Prints the command output
+    return  True
